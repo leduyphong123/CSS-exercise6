3ef2d6cfe575984f7fa15b6f0b98a518a5b7c384
leduyphong123 <leduyphongck789@gmail.com>
2023-06-29 15:45:20 +0700

    baitap4

---
+++ baitap4/index.js
@@ -0,0 +1,23 @@
+let ad = document.getElementById('ad');
+ad.addEventListener('click',()=>{
+    let ad_p = document.getElementById('ad_p');
+    window.scrollTo(0,ad_p.offsetTop);
+});
+
+let ph = document.getElementById('ph');
+ph.addEventListener('click',()=>{
+    let ph_p = document.getElementById('ph_p');
+    window.scrollTo(0,ph_p.offsetTop);
+});
+
+let ld = document.getElementById('ld');
+ld.addEventListener('click',()=>{
+    let ld_p = document.getElementById('ld_p');
+    window.scrollTo(0,ld_p.offsetTop);
+});
+
+let cg = document.getElementById('cg');
+cg.addEventListener('click',()=>{
+    let cg_p = document.getElementById('cg_p');
+    window.scrollTo(0,cg_p.offsetTop);
+});
